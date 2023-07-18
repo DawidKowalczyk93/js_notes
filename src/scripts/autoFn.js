6fb7ecad44bd83514be31f6ed997a28c85071fbf
DawidKowalczyk93 <bandiwaves@gmail.com>
2023-07-18 14:59:02 +0200

    oop style

--- src/scripts/autoFn.js
+++ src/scripts/autoFn.js
@@ -64,45 +64,102 @@
 // React jest tak zbudowany, żeby
 
 // 2 opcja w której używamy this. Ten kod jest cięzki
-function Auto(brand, maxSpeed) {
+// function Auto(brand, maxSpeed) {
+//     this.brand = brand;
+//     this.speed = 0;
+//     this.maxSpeed = maxSpeed;
+//     this.engine = false;
+// }
+//
+//
+//
+// Auto.prototype.startEngine = function () {
+//     if (!this.engine) {
+//         this.engine = true;
+//         console.log('silnik odpalony');
+//     } else {
+//         console.log('silnik już był odpalony');
+//     }
+// }
+//
+// Auto.prototype.stopEngine = function () {
+//     if (this.speed === 0) {
+//         this.engine = false;
+//         console.log('silnik zgaszony')
+//     } else {
+//         console.log('zatrzymaj się wpierw, zanim zgasisz silnik');
+//     }
+// }
+//
+// Auto.prototype.speedUp = function (amount) {
+//     if (this.engine) {
+//         this.speed = Math.min(this.speed + amount, this.maxSpeed);
+//         console.log(`Twoja prędkość to: ${this.speed}`);
+//     } else {
+//         console.log('odpal silnik wpierw');
+//     }
+// }
+//
+// Auto.prototype.slowDown = function (amount) {
+//     this.speed = Math.max(this.speed - amount, 0);
+//     console.log(`Twoja prędkość to: ${this.speed}`);
+// }
+//
+// const bmw = new Auto('bmw', 140);
+// const mercedes = new Auto('mercedes', 190);
+//
+// bmw.speedUp(200);
+// bmw.speedUp(200);
+// bmw.startEngine();
+// bmw.speedUp(40);
+// bmw.speedUp(40);
+// bmw.speedUp(240);
+// bmw.stopEngine();
+// bmw.slowDown(50);
+// bmw.slowDown(250);
+// bmw.stopEngine();
+
+//classy
+class Auto {
+    constructor(brand, maxSpeed) {
     this.brand = brand;
     this.speed = 0;
     this.maxSpeed = maxSpeed;
     this.engine = false;
-}
-
-
-
-Auto.prototype.startEngine = function () {
-    if (!this.engine) {
-        this.engine = true;
-        console.log('silnik odpalony');
-    } else {
-        console.log('silnik już był odpalony');
     }
-}
 
-Auto.prototype.stopEngine = function () {
-    if (this.speed === 0) {
-        this.engine = false;
-        console.log('silnik zgaszony')
-    } else {
-        console.log('zatrzymaj się wpierw, zanim zgasisz silnik');
+    startEngine() {
+        if (!this.engine) {
+            this.engine = true;
+            console.log('silnik odpalony');
+        } else {
+            console.log('silnik już był odpalony');
+        }
     }
-}
 
-Auto.prototype.speedUp = function (amount) {
-    if (this.engine) {
-        this.speed = Math.min(this.speed + amount, this.maxSpeed);
+    stopEngine() {
+        if (this.speed === 0) {
+            this.engine = false;
+            console.log('silnik zgaszony')
+        } else {
+            console.log('zatrzymaj się wpierw, zanim zgasisz silnik');
+        }
+    }
+
+    speedUp(amount) {
+        if (this.engine) {
+            this.speed = Math.min(this.speed + amount, this.maxSpeed);
+            console.log(`Twoja prędkość to: ${this.speed}`);
+        } else {
+            console.log('odpal silnik wpierw');
+        }
+    }
+
+    slowDown(amount) {
+        this.speed = Math.max(this.speed - amount, 0);
         console.log(`Twoja prędkość to: ${this.speed}`);
-    } else {
-        console.log('odpal silnik wpierw');
     }
-}
 
-Auto.prototype.slowDown = function (amount) {
-    this.speed = Math.max(this.speed - amount, 0);
-    console.log(`Twoja prędkość to: ${this.speed}`);
 }
 
 const bmw = new Auto('bmw', 140);
